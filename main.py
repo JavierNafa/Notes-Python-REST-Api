5c084d9f24859df8440c580e80fdb37f363d6f86
JavierNafa <javier.navarretefacio@gmail.com>
2021-01-11 16:02:27 -0600

    Flask_port and flask_debug were added as environment variables

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 from app import app
+from ast import literal_eval
+from settings import flask_debug, flask_port
 from src.database.mongo import connect
 from celery_config import configure_celery
 
@@ -9,4 +11,5 @@ app.celery = celery
 if __name__ == '__main__':
     with app.app_context():
         connect()
-    app.run(port=8000, debug=True, use_reloader=False, threaded=True)
+    app.run(port=flask_port, debug=literal_eval(flask_debug),
+            use_reloader=False, threaded=True)
